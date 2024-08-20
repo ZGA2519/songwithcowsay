a810c13496413e23700d5c3545a5f27b677c9cd7
ZGA2519 <phuripat2519@gmail.com>
2024-08-20 20:18:11 +0700

    Add ฝนตกไหม

---
+++ ฝนตกไหม.sh
@@ -0,0 +1,50 @@
+if command -v cowsay &> /dev/null; then
+    clear
+    cowsay "(⸝⸝⸝>﹏<⸝⸝⸝)"
+    sleep 1
+    clear
+    cowsay "แค่อยากจะรู้ว่าตรงที่เธอยืนนั้น"
+    sleep 5
+    clear
+    cowsay "มีฝนตกไหม"
+    sleep 2.8
+    clear
+    cowsay "สบายดีไหม"
+    sleep 2.8
+    clear
+    cowsay "เธอกลัวฟ้าร้องหรือเปล่า"
+    sleep 3.1
+    clear
+    cowsay "ถ้าหากตรงนั้นไม่มีใคร"
+    sleep 4.1
+    clear
+    cowsay "ฉันพร้อม ฉันพร้อมจะไป"
+    sleep 5.4
+    clear
+    cowsay "แต่ฉันก็รู้ดีว่าข้างข้างเธอนั้น"
+    sleep 4.3
+    clear
+    cowsay "มีเขาใช่ไหม"
+    sleep 3.2
+    clear
+    cowsay "และคนคนนั้น"
+    sleep 3
+    clear
+    cowsay "ดูแลเธอดีหรือเปล่า"
+    sleep 3.3
+    clear
+    cowsay "ถ้าเธอยิ้มฉันก็ดีใจ"
+    sleep 3.7
+    clear
+    cowsay "แค่ตรงนี้ฉันไม่มีใคร"
+    sleep 5.9
+    clear
+    cowsay "ในคืนที่ฝนโปรยลงมา"
+    sleep 5.5
+    clear
+    cowsay "(⸝⸝⸝>﹏<⸝⸝⸝)"
+    echo fin
+    sleep 5
+else
+    echo "Cowsay is required, Please install"
+fi
